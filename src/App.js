2d59b4d6ff923187712796869b5b3022203fdff3
arif034 <45455917+arif034@users.noreply.github.com>
2022-01-22 22:25:30 +0530

    more css practise more css practise

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <div className="a"></div>
-      <div className="b"></div>
-      <div className="c"></div>
+      <div className="a">
+        <div className="b"></div>
+        <div className="c"></div>
+      </div>
     </div>
   );
 }
