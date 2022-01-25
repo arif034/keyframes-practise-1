32d333ffc436bcfac5377444c34b96fbc155d388
arif034 <45455917+arif034@users.noreply.github.com>
2022-01-25 09:45:05 +0530

    changes changes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <div className="a">
+      <div className="grid-container">
         <div className="b"></div>
         <div className="c"></div>
+        <div className="d"></div>
+        <div className="e"></div>
       </div>
     </div>
   );
